6e1f531f1766ff9f73a1781f4489b22678f75fc4
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-06-26 00:02:16 +0530

    web init

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import keras
+from keras.models import load_model
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+st.header('Flower Classification CNN Model')
+
+flower_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+
+model = load_model('Flower_Recog_Model.keras')
+
+def classify_images(image_path):
+    input_image = tf.keras.utils.load_img(image_path, target_size=(180,180))
+    input_image_array = tf.keras.utils.img_to_array(input_image)
+    input_image_exp_dim = tf.expand_dims(input_image_array,0)
+
+    predections = model.predict(input_image_exp_dim)
+    result = tf.nn.softmax(predections[0])
+    outcome = 'The Image Belongs to ' + flower_names[np.argmax(result)] + ' with a score of ' + str(np.max(result)*100)
+    return outcome
+
+uploaded_file = st.file_uploader('upload an image')
+if uploaded_file is not None:
+    with open(os.path.join('upload', uploaded_file.name), 'wb') as f:
+        f.write(uploaded_file.getbuffer())
